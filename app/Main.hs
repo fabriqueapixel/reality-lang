6e53db5e19e6fe5644daae8d201bb2dd0afad5d1
Thomas Vergne <contact@thomas-vergne.fr>
2026-06-24 18:39:53 +0200

    fix(main): fixed compiler warnings inside main file

--- app/Main.hs
+++ app/Main.hs
@@ -1,155 +1,189 @@
 module Main where
 
 import Control.Monad.Result
+import Data.Text qualified as Text
 import Language.Reality.Backend.ANF.Converter qualified as ANF
 import Language.Reality.Backend.Closure.Converter qualified as CC
 import Language.Reality.Backend.Codegen qualified as CG
 import Language.Reality.Backend.Specialization.Resolver qualified as SR
 import Language.Reality.Frontend.Import.Resolver qualified as IR
 import Language.Reality.Frontend.Module.Resolver qualified as MR
-import Language.Reality.Frontend.Parser hiding (parseError, Parser)
+import Language.Reality.Frontend.Parser hiding (Parser, parseError)
 import Language.Reality.Frontend.Parser.Toplevel qualified as T
 import Language.Reality.Frontend.Typechecker.Checker qualified as TC
+import Options.Applicative qualified as App
 import System.Directory
+import System.Environment (getEnv)
 import System.FilePath
 import System.Process
 
-import Options.Applicative qualified as App
-import qualified Data.Text as Text
-import System.Environment (getEnv)
-
 data BuildOptions
-    = MkBuildRealityOptions
-        { optInputFile :: FilePath
-        , optPathAliases :: [(Text, FilePath)]
-        , optHeaders :: [FilePath]
-        }
-    | MkBuildClangOptions
-        { clangInputFile :: FilePath
-        , clangOutputFile :: FilePath
-        , clangLibraries :: [FilePath]
-        , clangFlags :: [String]
-        }
+  = MkBuildRealityOptions
+      { optInputFile :: FilePath,
+        optPathAliases :: [(Text, FilePath)],
+        optHeaders :: [FilePath]
+      }
+  | MkBuildClangOptions
+      { clangInputFile :: FilePath,
+        clangInputBackend :: Text,
+        clangOutputFile :: FilePath,
+        clangLibraries :: [FilePath],
+        clangFlags :: [String]
+      }
 
 options :: App.Parser BuildOptions
-options = MkBuildRealityOptions
+options =
+  MkBuildRealityOptions
     <$> App.strArgument
-        ( App.metavar "FILE"
-       <> App.help "Reality source file to compile" )
-    <*> App.many (App.option (App.eitherReader parseAlias)
-        ( App.long "path-alias"
-       <> App.short 'p'
-       <> App.metavar "ALIAS=PATH"
-       <> App.help "Path alias for import resolution" ))
-    <*> App.many (App.strOption
-        ( App.long "header"
-       <> App.short 'I'
-       <> App.metavar "HEADER"
-       <> App.help "Additional header file to include in the generated C code" ))
+      ( App.metavar "FILE"
+          <> App.help "Reality source file to compile"
+      )
+    <*> App.many
+      ( App.option
+          (App.eitherReader parseAlias)
+          ( App.long "path-alias"
+              <> App.short 'p'
+              <> App.metavar "ALIAS=PATH"
+              <> App.help "Path alias for import resolution"
+          )
+      )
+    <*> App.many
+      ( App.strOption
+          ( App.long "header"
+              <> App.short 'I'
+              <> App.metavar "HEADER"
+              <> App.help "Additional header file to include in the generated C code"
+          )
+      )
   where
     parseAlias :: String -> Either String (Text, FilePath)
     parseAlias s =
-        case break (== '=') s of
-            (alias, '=':path) -> Right (Text.pack alias, path)
-            _ -> Left "Alias must be in the format ALIAS=PATH"
+      case break (== '=') s of
+        (alias, '=' : path) -> Right (Text.pack alias, path)
+        _ -> Left "Alias must be in the format ALIAS=PATH"
 
 clangOptions :: App.Parser BuildOptions
-clangOptions = MkBuildClangOptions
+clangOptions =
+  MkBuildClangOptions
     <$> App.strArgument
-        ( App.metavar "INPUT"
-       <> App.help "Input C file to compile" )
+      ( App.metavar "INPUT"
+          <> App.help "Input C file to compile"
+      )
     <*> App.strOption
-        ( App.long "output"
-       <> App.short 'o'
-       <> App.metavar "OUTPUT"
-       <> App.help "Output executable file name" 
-       <> App.value "output/program")
-    <*> App.many (App.strOption
-        ( App.long "library"
-       <> App.short 'l'
-       <> App.metavar "LIBRARY"
-       <> App.help "Additional library to link against" ))
-    <*> App.many (App.strOption
-        ( App.long "flag"
-       <> App.short 'f'
-       <> App.metavar "FLAG"
-       <> App.help "Additional flag to pass to the C compiler" ))
+      ( App.metavar "BACKEND"
+          <> App.help "Backend used to generate the C file (e.g., 'wasm', 'native')"
+          <> App.value "native"
+          <> App.showDefault
+          <> App.long "backend"
+          <> App.short 'b'
+      )
+    <*> App.strOption
+      ( App.long "output"
+          <> App.short 'o'
+          <> App.metavar "OUTPUT"
+          <> App.help "Output executable file name"
+          <> App.value "output/program"
+      )
+    <*> App.many
+      ( App.strOption
+          ( App.long "library"
+              <> App.short 'l'
+              <> App.metavar "LIBRARY"
+              <> App.help "Additional library to link against"
+          )
+      )
+    <*> App.many
+      ( App.strOption
+          ( App.long "flag"
+              <> App.short 'f'
+              <> App.metavar "FLAG"
+              <> App.help "Additional flag to pass to the C compiler"
+          )
+      )
+
 main :: IO ()
 main = do
-    buildOpts <- App.execParser (App.info p App.idm)
-    buildOutput buildOpts
+  buildOpts <- App.execParser (App.info p App.idm)
+  buildOutput buildOpts
   where
-    p = App.hsubparser
-        ( App.command "c" cOpts )
-        <|> options 
+    p =
+      App.hsubparser
+        (App.command "c" cOpts)
+        <|> options
 
-    cOpts = App.info (App.helper <*> clangOptions)
-      ( App.fullDesc
-     <> App.progDesc "Compile a Reality output C file to an executable using C compiler"
-     <> App.header "hello - a test for optparse-applicative" )
-
+    cOpts =
+      App.info
+        (App.helper <*> clangOptions)
+        ( App.fullDesc
+            <> App.progDesc "Compile a Reality output C file to an executable using C compiler"
+            <> App.header "hello - a test for optparse-applicative"
+        )
 
 buildOutput :: BuildOptions -> IO ()
 buildOutput (MkBuildRealityOptions inputFile pathAliases headers) = do
-    let file = inputFile
-        cwd  = takeDirectory file
+  let file = inputFile
+      cwd = takeDirectory file
 
-    fileContent :: Text <- decodeUtf8 <$> readFileBS file
+  writeIORef MR.cwdGlobal cwd
 
-    result <- parseRealityFile file fileContent T.parseProgram
+  fileContent :: Text <- decodeUtf8 <$> readFileBS file
 
-    let pathAliases' = fromList pathAliases
-    pathAliases'' <- mapM makeAbsolute pathAliases'
+  result <- parseRealityFile file fileContent T.parseProgram
 
-    case result of
-        Right ast -> do
-            let pipeline =
-                    IR.runImportResolver cwd pathAliases''
-                        |> MR.runModuleResolver
-                        |> TC.runTypechecker
-                        |> SR.runSpecializationResolver
-                        |> CC.convertProgram
-                        |> ANF.convertToANF
-                        |> CG.codegenProgram
+  let pathAliases' = fromList pathAliases
+  pathAliases'' <- mapM makeAbsolute pathAliases'
 
-            pipelineResult <- runExceptT $ pipeline ast
+  case result of
+    Right ast -> do
+      let pipeline =
+            IR.runImportResolver cwd pathAliases''
+              |> MR.runModuleResolver
+              |> TC.runTypechecker
+              |> SR.runSpecializationResolver
+              |> CC.convertProgram
+              |> ANF.convertToANF
+              |> CG.codegenProgram
 
-            let includes = ["<stdint.h>", "<stdbool.h>", "<pthread.h>", "<gc.h>"]
-            let headers' = map (\h -> do
-                        let path = toText h
-                        if Text.isPrefixOf "<" path && Text.isSuffixOf ">" path
-                            then "#include " <> path
-                            else "#include \"" <> path <> "\""
-                    ) headers
-            let defines = ["#define GC_THREADS"]
+      pipelineResult <- runExceptT $ pipeline ast
 
-            handle pipelineResult $ \cstr -> do
-                let finalCstr =
-                        unlines defines
-                        <> unlines (map ("#include " <>) includes)
-                        <> unlines headers'
-                        <> "\n\n"
-                        <> cstr
+      let includes = ["<stdint.h>", "<stdbool.h>", "<pthread.h>", "<gc.h>"]
+      let headers' =
+            map
+              ( \h -> do
+                  let path = toText h
+                  if Text.isPrefixOf "<" path && Text.isSuffixOf ">" path
+                    then "#include " <> path
+                    else "#include \"" <> path <> "\""
+              )
+              headers
+      let defines = ["#define GC_THREADS"]
 
-                writeFileText (file -<.> "c") finalCstr
-        Left err -> do
-            parseError err file (Just fileContent)
-buildOutput (MkBuildClangOptions inputFile outputFile libraries flags) = do
-    realityDir <- getEnv "REALITY_DIR"
-    libgcDir <- getEnv "LIBGC"
+      handle pipelineResult $ \cstr -> do
+        let finalCstr =
+              unlines defines
+                <> unlines (map ("#include " <>) includes)
+                <> unlines headers'
+                <> "\n\n"
+                <> cstr
 
-    let clangCmd = "clang"
-        args = 
-            [inputFile, "-o", outputFile] 
-            ++ libraries 
-            ++ [
-                realityDir <> "/libc/String.c", 
-                realityDir <> "/libc/Actor.c", 
-                libgcDir <> "/lib/libgc.a", 
-                "-I" <> libgcDir <> "/include/", 
-                "-w"
-            ]
-            ++ map ('-':) flags
+        writeFileText (file -<.> "c") finalCstr
+    Left err -> do
+      parseError err file (Just fileContent)
+buildOutput (MkBuildClangOptions inputFile _ outputFile libraries flags) = do
+  realityDir <- getEnv "REALITY_DIR"
+  libgcDir <- getEnv "LIBGC"
 
-    ppBuild $ "Running clang with arguments: " <> unwords (map fromString args)
-    callProcess clangCmd args
+  let clangCmd = "clang"
+      args =
+        [inputFile, "-o", outputFile]
+          ++ libraries
+          ++ [ realityDir <> "/libc/String.c",
+               realityDir <> "/libc/Actor.c",
+               libgcDir <> "/lib/libgc.a",
+               "-I" <> libgcDir <> "/include/",
+               "-w"
+             ]
+          ++ map ('-' :) flags
+
+  ppBuild $ "Running clang with arguments: " <> unwords (map fromString args)
+  callProcess clangCmd args
